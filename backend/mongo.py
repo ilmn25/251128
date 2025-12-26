c63c3bd579e98778bb2ccb47e4cdeaa7f988a1c0
illu <kilmn025@gmail.com>
2025-12-27 01:25:17 +0800

    Added mongo.py

---
+++ backend/mongo.py
@@ -0,0 +1,10 @@
+﻿from pymongo.mongo_client import MongoClient
+from pymongo.server_api import ServerApi
+import env
+
+client = MongoClient(env.MONGO_URI, server_api=ServerApi('1'))
+try:
+    client.admin.command('ping')
+    print("Pinged your deployment. You successfully connected to MongoDB!")
+except Exception as e:
+    print(e)
